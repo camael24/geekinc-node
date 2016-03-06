8c03f90d79afa14b35f0d994c94630ee14d34233
camael24 <thehawk@hoa-project.net>
2016-03-06 14:20:33 +0100

    chained element

--- public/js/client.js
+++ public/js/client.js
@@ -1,13 +1,14 @@
 var socket    = io.connect();
 var title     = $('#title');
 var color     = title.css('background-color');
-var tl        = new TimelineMax();
+var root      = new TimelineMax();
 
 title.css('transform', 'scaleX(0)');
 
 function hide() {
   TweenLite.to(title, 1, {transform: 'scaleX(0)', "transform-origin": 'right', color: color});
 }
+
 function show(t, h, tl) {
   title.text(t);
   TweenLite.to(title, 1, {transform: 'scaleX(1)', "transform-origin": 'left', color: 'white'});
@@ -18,11 +19,12 @@ function show(t, h, tl) {
   }
 }
 
-
 socket.on('display_title', function (o){
 
   console.log(o)
-  tl.progress(1); // TODO : this not take about the ended :/
+  tl = new TimelineMax();
   tl.add(hide);
   tl.addPause('+=1', show, [o.title, o.duration, tl]);
+
+  root.add(tl);
 });
