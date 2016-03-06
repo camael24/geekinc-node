fb287be038b778af466b9e86f38c39a190b41b34
camael24 <thehawk@hoa-project.net>
2016-03-06 14:45:48 +0100

    change dynamiclly the class

--- public/js/client.js
+++ public/js/client.js
@@ -9,8 +9,11 @@ function hide() {
   TweenLite.to(title, 1, {transform: 'scaleX(0)', "transform-origin": 'right', color: color});
 }
 
-function show(t, h, tl) {
+function show(t, c, h, tl) {
   title.text(t);
+  if(c != 'none') {
+    title.attr('class', 'text-'+c)
+  }
   TweenLite.to(title, 1, {transform: 'scaleX(1)', "transform-origin": 'left', color: 'white'});
 
   if(h && h > 0) {
@@ -24,7 +27,13 @@ socket.on('display_title', function (o){
   console.log(o)
   tl = new TimelineMax();
   tl.add(hide);
-  tl.addPause('+=1', show, [o.title, o.duration, tl]);
+
+  var c = 'none'
+  if(o.class && o.class != '') {
+    c = o.class
+  }
+
+  tl.addPause('+=1', show, [o.title, c, o.duration, tl]);
 
   root.add(tl);
 });
